b9030c7d4fd485aa9aedb64d4523afa81bf1c354
Carlos Falgueras García <fark.zano@gmail.com>
2015-05-03 03:11:00 +0200

    Added more tests.

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "world.h"
 
+void printCells(struct World *world);
+
 int main(int nargs, char *argv[])
 {
 	struct World *world = NULL;
@@ -8,8 +10,6 @@ int main(int nargs, char *argv[])
 
 	world = createWorld(50, 50);
 	printf("Mundo creado = %p\n", world);
-	clearWorld(world);
-	printf("Mundo inicializado\n");
 
 	printf("Añadiendo célula\n");
 	cell = addNewCell(4, 4, world);
@@ -28,7 +28,36 @@ int main(int nargs, char *argv[])
 		getCell(1, 1, world) != NULL? "alive" : "dead");
 
 
+	addNewCell(1, 2, world);
+	addNewCell(1, 3, world);
+	addNewCell(1, 3, world);
+	addNewCell(2, 1, world);
+	addNewCell(2, 2, world);
+	addNewCell(2, 3, world);
+	addNewCell(4, 3, world);
+	addNewCell(9, 9, world);
+
+	printCells(world);
+
+	clearWorld(world);
+	printf("Mundo borrado: \n");
+
 	destroyWorld(world);
 
 	return 0;
+}
+
+void printCells(struct World *world)
+{
+	int i = 0;
+	wsize_t x, y;
+	struct Cell *cell;
+
+	for (cell = wit_first(world);
+	     wit_done(cell, world);
+	     cell = wit_next(cell))
+	{
+		getCellPos(&x, &y, cell);
+		printf("%d - (%lu, %lu)\n", i++, x, y);
+	}
 }
