a6063d46894a5c23bc161d0b70e098650e36314f
Carlos Falgueras García <fark.zano@gmail.com>
2015-06-01 17:29:51 +0200

    Improved viewer.

--- viewer.sh
+++ viewer.sh
@@ -8,21 +8,57 @@ nNodes=$#
 nIterations=($1/*)
 nIterations=${#nIterations[@]}
 directories=($@)
-iterations=(node?/*)
+
+for i in ${directories[@]}; do
+	iterations+=($i/*)
+done
 nFiles=${#iterations[@]}
 
 for i in $(seq 0 $((nIterations-1))); do
 	for j in $(seq 0 $((nNodes-1))); do
 		indx=$((i+j*nIterations))
-		orderedIt=(${orderedIt[@]} ${iterations[$indx]})
+		orderedIt+=(${iterations[$indx]})
 	done
 done
 
+i=0
+quit=0
+period="0.5"
+rperiod="-t$period"
+pause="       "
 clear
-for i in $(seq 0 $nNodes $((nFiles-1))); do
-	echo ${orderedIt[@]:$i:$nNodes}
+while [ $quit -eq 0 ]; do
+	echo -e "${orderedIt[@]:$i:$nNodes}\tPeriod = $period    \t$pause\n"
 	cat ${orderedIt[@]:$i:$nNodes}
-	sleep 0.1
+
+	read -sr -n1 $rperiod key
+
+	if [ "$key" = "b" ]; then
+		(( i -= $nNodes*2 ))
+		[ $i -lt "-$nNodes" ] && (( i = $nFiles - $nNodes*2 ))
+	elif [ "$key" = "r" ]; then
+		(( i = -$nNodes ))
+	elif [ "$key" = "p" ]; then
+		if [ $rperiod ]; then
+			rperiod=
+			pause="[PAUSE]"
+		else
+			rperiod="-t$period"
+			pause="       "
+		fi
+	elif [ "$key" = "-" ]; then
+		period=$(echo "$period + 0.1" | bc)
+		rperiod="-t$period"
+	elif [ "$key" = "+" ]; then
+		period=$(echo "if($period > 0.1) $period - 0.1" else $period | bc)
+		period=$(echo "if($period > 0.01) $period - 0.01" else $period | bc)
+		rperiod="-t$period"
+	elif [ "$key" = "q" ]; then
+		quit=1
+	fi
+
+	(( i = (i + $nNodes)%$nFiles ))
+
 	setCursor 0 0
 done
-# clear
+clear
